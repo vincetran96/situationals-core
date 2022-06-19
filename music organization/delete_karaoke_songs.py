1498d5bab14bec5cc14eff18165dda673b822330
Vince Tran <vietanhtran.96@gmail.com>
2022-06-19 18:01:12 -0500

    Add delete karaoke songs function

---
+++ music organization/delete_karaoke_songs.py
@@ -0,0 +1,21 @@
+'''Delete karaoke songs
+'''
+
+from pathlib import Path
+from typing import Set
+
+
+MUSIC_PATH = Path(r"E:\music")
+
+# Search for Karaoke
+to_delete_file_paths: Set[Path] = set()
+for file_path in MUSIC_PATH.rglob("*.mp3"):
+    if file_path.is_file():
+        base_name = file_path.name.lower()
+        if base_name.find("karaoke") > -1:
+            to_delete_file_paths.add(file_path)
+
+# Remove
+for file_path in to_delete_file_paths:
+    file_path.unlink()
+    print(f"Deleted {file_path}")
